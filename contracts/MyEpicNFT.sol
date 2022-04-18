bc73e8a68df9fb24ced46a9de848463bf74d5775
Shrey Jain <shrey.jain711@gmail.com>
2022-04-18 19:12:19 +0530

    clean up and initialised contract

---
+++ contracts/MyEpicNFT.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.1;
+
+import "hardhat/console.sol";
+
+contract MyEpicNFT {
+    constructor() {
+        console.log("Finally making my NFT contract!!");
+    }
+}
